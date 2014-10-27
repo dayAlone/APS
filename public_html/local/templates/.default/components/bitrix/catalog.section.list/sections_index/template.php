6b78284c05f7d49e4ff4409f810b94d821f35a8f
dayAlone <andrey.slider@gmail.com>
2014-10-28 00:47:13 +0500

    Sections

---
+++ public_html/local/templates/.default/components/bitrix/catalog.section.list/sections_index/template.php
@@ -0,0 +1,17 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+$this->setFrameMode(true);
+if(count($arResult['SECTIONS'])>0):
+?>
+<div class="sections">
+	<div class="container">
+		<?foreach ($arResult['SECTIONS'] as $key => &$item):?>
+		<a href="#" class="sections__item">
+    		<div class="sections__image">
+    			<?=file_get_contents($_SERVER['DOCUMENT_ROOT'].CFile::GetPath($item['UF_SVG']))?>
+    		</div>
+    		<div class="sections__text"><?=$item['NAME']?></div>
+    	</a>
+    	<?endforeach;?>
+    </div>
+</div>
+<?endif;?>
