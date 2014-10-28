fd5acb5c49c8b9b57996343a09a8b435288942ee
dayAlone <andrey.slider@gmail.com>
2014-10-29 02:13:55 +0500

    About

---
+++ public_html/about/index.php
@@ -0,0 +1,39 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle('О компании');
+$APPLICATION->SetPageProperty('body_class', "about");
+?>
+<h1 class="page__title">О компании</h1>
+<div class="page__divider s-margin-top xl-margin-bottom"></div>
+<h2 class="page__sub-title xl-margin-bottom">Аргус СварСервис – компания экспертов  в области сварочно-монтажных работы при строительстве и ремонте трубопроводов мирового уровня.</h2>
+<p class="xl-margin-bottom">Более 30 лет предприятие занимается поставкой оборудования и материалов для различных видов сварки, антикоррозионной защиты трубопроводов, контроля качества, испытаний, диагностики и ремонта труб, а также для таких видов работы с трубами, как укладка, такелажные работы, резка и обработка кромок, плазменная резка, индукционный подогрев, выравнивание стыков и гнутьё.</p>
+<h2 class="page__sub-title xl-margin-bottom">Компания является дистрибьютором техники таких всемирно известных марок, как:</h2>
+<div class="partners">
+  <div class="row">
+    <div class="col-xs-3"><a href="#" class="partners__item">
+        <div class="partners__image"><img src="/layout/images/lg-1.jpg"></div>
+        <div class="partners__text">сварочное оборудование и материалы</div></a></div>
+    <div class="col-xs-3"><a href="#" class="partners__item">
+        <div class="partners__image"><img src="/layout/images/lg-2.jpg"></div>
+        <div class="partners__text">автоматические сварочные комплексы</div></a></div>
+    <div class="col-xs-3"><a href="#" class="partners__item">
+        <div class="partners__image"><img src="/layout/images/lg-3.jpg"></div>
+        <div class="partners__text">строительная и землеройная техника</div></a></div>
+    <div class="col-xs-3"><a href="#" class="partners__item">
+        <div class="partners__image"><img src="/layout/images/lg-4.jpg"></div>
+        <div class="partners__text">сварочные тракторы и трубоукладчики</div></a></div>
+    <div class="col-xs-4"><a href="#" class="partners__item">
+        <div class="partners__image"><img src="/layout/images/lg-5.jpg"></div>
+        <div class="partners__text">изоляционные материалы</div></a></div>
+    <div class="col-xs-4"><a href="#" class="partners__item">
+        <div class="partners__image"><img src="/layout/images/lg-6.jpg"></div>
+        <div class="partners__text">установки горизонтально-наклонного бурения</div></a></div>
+    <div class="col-xs-4"><a href="#" class="partners__item">
+        <div class="partners__image"><img src="/layout/images/lg-7.jpg"></div>
+        <div class="partners__text">портативные установки для резки и обработки торцов труб</div></a></div>
+  </div>
+</div>
+<h2 class="page__sub-title">Компания «Аргус» нацелена на долгосрочное стратегическое партнёрство, поэтому главные принципы её работы – поддержка заказчика на всех этапах проекта и совместный поиск оптимальных решений.</h2>
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
