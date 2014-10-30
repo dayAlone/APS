de898513bfd3e3d6f4c67a87ba13e4d5f46cd298
dayAlone <andrey.slider@gmail.com>
2014-10-31 00:33:50 +0500

    Search

--- public_html/search/index.php
+++ public_html/search/index.php
@@ -3,7 +3,7 @@ require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetPageProperty('body_class', "search");
 ?> 
 <h1 class="inline-block page__title">Поиск</h1>
-<div class="page__divider"></div>
+<div class="page__divider page__divider--blue l-margin-top xl-margin-bottom"></div>
 <?
 $APPLICATION->IncludeComponent("bitrix:search.page","",Array(
     "TAGS_SORT" => "NAME",
