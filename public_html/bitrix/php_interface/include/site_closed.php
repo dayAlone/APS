bc931c1e7d98bf11c91caf10abc13385a919d6fa
dayAlone <andrey.slider@gmail.com>
2014-10-31 18:51:11 +0500

    Logo

---
+++ public_html/bitrix/php_interface/include/site_closed.php
@@ -0,0 +1,25 @@
+<html><head>
+	<title>Аргус пайплайн сервис</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			float: left;
+			width: 100%;
+			height: 100%;
+			background: #0055a5 url("/layout//images/bg.jpg") center center no-repeat;
+			background-size: cover;
+			position: relative;
+		}
+		#logo {
+			position: absolute;
+			top:50%;
+			left: 50%;
+			margin-top: -146px;
+			margin-left: -177px;
+		}
+	</style></head>
+<body>
+	<img src="/layout/images/logo_rus.png" alt="" id="logo">
+
+</body></html>
